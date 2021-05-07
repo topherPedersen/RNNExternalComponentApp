b0367f569d082bc7a0d33677e801ada264506203
Christopher Pedersen <chris@topherpedersen.com>
2021-05-07 09:36:08 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>RNNExternalComponentApp</Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default App;
